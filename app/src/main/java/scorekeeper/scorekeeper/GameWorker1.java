4af9523574e950defbbad3512ed7f0b00061b3ae
Brandon Haskett <brhaskett@yahoo.com>
2019-05-30 23:30:01 -0400

    updating to fix issue with files being corrupt

---
+++ app/src/main/java/scorekeeper/scorekeeper/GameWorker1.java
@@ -0,0 +1,34 @@
+package scorekeeper.scorekeeper;
+
+public class GameWorker1 {
+
+    public static String AddPoints(int currentPoints)
+    {
+        int newPointValue = currentPoints + 1;
+        return Integer.toString(newPointValue);
+    }
+
+    public static String SubtractPoints(int currentPoints)
+    {
+        if (currentPoints == 0)
+        {
+            return "0";
+        }
+        int newPointValue = currentPoints - 1;
+        return Integer.toString(newPointValue);
+    }
+
+    public static String DecideTheWinner(int player1Score, int player2Score)
+    {
+        if(player1Score > player2Score)
+        {
+            return "Player 1 is the victor";
+        }
+        else if(player1Score == player2Score)
+        {
+            return "Game ends in a draw";
+        }
+
+        return "Player 2 is the victor";
+    }
+}
